7bda20d5d1c67e270daef9f35e89cb5f484d129a
ned <ned@mbp.local>
2020-12-15 15:43:50 +0100

    initial commit

---
+++ dl-buddy/Stuff/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  dl-buddy
+//
+//  Created by ned on 15/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
---
+++ dl-buddy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  dl-buddy
+//
+//  Created by ned on 15/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBAction func addDownloadButtonTapped(sender: NSToolbarItem) {
+        print("add button tapped")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
