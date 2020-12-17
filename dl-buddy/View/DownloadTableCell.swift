86377b7b89feef1bc1a236cb09c8ba2fb84d680d
ned <ned@mbp.local>
2020-12-18 00:15:45 +0100

    added initial NSTableView and sample cell

---
+++ dl-buddy/View/DownloadTableCell.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadTableCell.swift
+//  dl-buddy
+//
+//  Created by ned on 17/12/20.
+//
+
+import Cocoa
+
+class DownloadTableCell: NSTableCellView {
+
+    @IBOutlet weak var sampleLabel: NSTextField!
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
